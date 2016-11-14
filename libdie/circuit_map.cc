992213118c1539159d760dac80f2e0d0f2ae8121
William D. Jones <thor0505@comcast.net>
2016-11-14 08:50:27 -0500

    Fix circuit_map's copy of file mapping functions.

--- libdie/circuit_map.cc
+++ libdie/circuit_map.cc
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <errno.h>
 #ifdef _WIN32
   #include <windows.h>
@@ -17,19 +18,20 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-static void map_file_ro(const char *fname, unsigned char *&data, long &size, bool accept_not_here)
+void map_file_ro(const char *fname, unsigned char *&data, int_least64_t &size, bool accept_not_here)
 {
   char msg[4096];
-  sprintf(msg, "Error opening %s for reading", fname);
   #ifdef _WIN32
     HANDLE fd = CreateFile(fname, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    if(fd == NULL) {
+    if(fd == INVALID_HANDLE_VALUE) {
       if(GetLastError() == ERROR_FILE_NOT_FOUND && accept_not_here) {
         data = 0;
         size = 0;
         return;
       }
-      perror(msg);
+      FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), msg, 4096, NULL);
+      fprintf(stderr, "CreateFile failed. Windows error code: %s", msg);
       exit(1);
     }
     size = GetFileSize(fd, NULL);
@@ -37,6 +39,7 @@ static void map_file_ro(const char *fname, unsigned char *&data, long &size, boo
     data = (unsigned char *) MapViewOfFile(map, FILE_MAP_READ, 0, 0, 0);
     CloseHandle(fd);
   #else
+    sprintf(msg, "Error opening %s for reading", fname);
     int fd = open(fname, O_RDONLY);
     if(fd < 0) {
       if(errno == ENOENT && accept_not_here) {
@@ -53,21 +56,38 @@ static void map_file_ro(const char *fname, unsigned char *&data, long &size, boo
   #endif
 }
 
-static void create_file_rw(const char *fname, unsigned char *&data, long size)
+void create_file_rw(const char *fname, unsigned char *&data, int_least64_t size)
 {
   char msg[4096];
-  sprintf(msg, "Error opening %s for writing", fname);
   #ifdef _WIN32
-    HANDLE fd = CreateFile(fname, GENERIC_READ|GENERIC_WRITE, 0, NULL, CREATE_ALWAYS|TRUNCATE_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    if(fd == NULL) {
-      perror(msg);
+    /* Try FILE_SHARED_READ if this fails. */
+    HANDLE fd = CreateFile(fname, GENERIC_READ|GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    if(fd == INVALID_HANDLE_VALUE) {
+      FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), msg, 4096, NULL);
+      fprintf(stderr, "CreateFile failed. Windows error code: %s", msg);
       exit(1);
     }
-    size = GetFileSize(fd, NULL);
-    HANDLE map = CreateFileMapping(fd, NULL, PAGE_READWRITE, 0, 0, fname);
+
+    LARGE_INTEGER full_size;
+    full_size.QuadPart = size;
+    HANDLE map = CreateFileMapping(fd, NULL, PAGE_READWRITE, full_size.HighPart, full_size.LowPart, fname);
+    if(map == NULL) {
+      FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), msg, 4096, NULL);
+      fprintf(stderr, "CreateFileMapping failed. Windows error code: %s", msg);
+      exit(1);
+    }
     data = (unsigned char *) MapViewOfFile(map, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+    if(data == NULL) {
+      FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), msg, 4096, NULL);
+      fprintf(stderr, "MapViewOfFile failed. Windows error code: %s", msg);
+      exit(1);
+    }
     CloseHandle(fd);
   #else
+    sprintf(msg, "Error opening %s for writing", fname);
     int fd = open(fname, O_RDWR|O_CREAT|O_TRUNC, 0666);
     if(fd < 0) {
       perror(msg);
@@ -90,7 +110,7 @@ circuit_map::circuit_map(const char *fname, int _nl, int _sx, int _sy, bool crea
     memset(map_adr, 0xff, long(nl)*sx*sy*4);
 
   } else {
-    long size;
+    int_least64_t size;
     map_file_ro(fname, map_adr, size, false);
   }
 
